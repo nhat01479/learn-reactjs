22c51b9a0fb73c4ccc9f9f8bef3d3be1a3a966d8
nhat01479 <nhat01479@gmail.com>
2023-08-23 11:49:23 +0700

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
 import React from 'react';
 import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Main from "./components/main/Main";
+
 import MainContent from './components/main/MainContent';
-import TodoFeature from "./features/Todo";
-import AlbumFeature from "./features/Song";
-import Register from "./features/Register/Register";
-import ProductList from "./features/Product/ProductList";
-import { Modal } from "bootstrap";
-import ModalCreate from "./components/main/ModalCreate";
+
 
 
 function App() {
